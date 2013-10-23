edaa2f726e818f114880fd1c100feee9abb8ba98
mwaidyanatha <mwaidyanatha@gmail.com>
2013-10-23 23:22:45 +0530

    Added default values to params.pp (appserver,elb,esb) Changed the user-management ldap settings

--- modules/elb/manifests/params.pp
+++ modules/elb/manifests/params.pp
@@ -25,44 +25,45 @@
 #
 
 class elb::params {
-  #$domain                     = 'domain.com'
-  #$elb_subdomain              = 'elb'
-  #$adminuser_name            =
-  #$adminuser_passwd          =
-  #$af_cluster_domain         =
-  #$af_identity_provider      =
-  #$af_keystore_alias         =
-  #$af_keystore_passwd        =
-  #$af_registration_link      =
-  #$am_subdomain              =
-  #$analyzer_subdomain        =
-  #$app_creation_delay        =
-  #$as_subdomain              =
-  #$auto_scaler_epr           =
-  #$auto_scaler_task_interval =
-  #$bpel_mysql_password       =
-  #$bpel_mysql_username       =
-  #$esb_subdomain             =
-  #$jenkins_apitoken          =
-  #$jenkins_resource_uid      =
-  #$keystore_password         =
-  #$management_subdomain      =
-  #$max_active                =
-  #$max_connections           =
-  #$max_wait                  =
-  #$mysql_driver_file         =
-  #$mysql_port                =
-  #$mysql_server_1            =
-  #$mysql_server_2            =
-  #$redmine_admin_passwd      =
-  #$redmine_admin_uname       =
-  #$registry_database         =
-  #$registry_password         =
-  #$registry_user             =
-  #$repository_type           =
-  #$server_startup_delay      =
-  #$ss_subdomain              =
-  #$depsync_svn_repo          =
-  #$svn_user                  =
-  #$svn_password              =
+  $package_repo         = 'http://downloads.wso2.com'
+  $depsync_svn_repo     = 'https://svn.appfactory.domain.com/wso2/repo/'
+  $local_package_dir    = '/mnt/packs'
+
+  # Service subdomains
+  $domain               = 'wso2.com'
+  $management_subdomain = 'management'
+
+  $admin_username       = 'admin'
+  $admin_password       = 'admin123'
+
+  # MySQL server configuration details
+  $mysql_server         = 'mysql.wso2.com'
+  $mysql_port           = '3306'
+  $max_connections      = '100000'
+  $max_active           = '150'
+  $max_wait             = '360000'
+
+  # Database details
+  $registry_user        = 'registry'
+  $registry_password    = 'ycJaCboyUo'
+  $registry_database    = 'governance'
+
+  $userstore_user       = 'userstore'
+  $userstore_password   = 'sUAKn09o5J'
+  $userstore_database   = 'userstore'
+
+  # Depsync settings
+  $svn_user             = 'wso2'
+  $svn_password         = 'wso2123'
+
+  # Auto-scaler
+  $auto_scaler_epr      = 'http://xxx:9863/services/AutoscalerService/'
+
+  #LDAP settings 
+  $ldap_connection_uri      = 'ldap://localhost:10389'
+  $bind_dn                  = 'uid=admin,ou=system'
+  $bind_dn_password         = 'adminpassword'
+  $user_search_base         = 'ou=system'
+  $group_search_base        = 'ou=system'
+  $sharedgroup_search_base  = 'ou=SharedGroups,dc=wso2,dc=org'
 }
